04ae85f2752ba83a1fe310a573adb942b4ecf7e1
Rahat Hossain <rahathossain690@gmail.com>
2020-08-09 22:53:32 +0600

    Updates

---
+++ precache-manifest.3e3e6be5f58f928aff52dbf144c5d033.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bea5a195f5e9f27e0ed0b2076d9d26",
+    "url": "/Anonym/index.html"
+  },
+  {
+    "revision": "e0e1791e210efc9a7b91",
+    "url": "/Anonym/static/css/main.d1c60c1c.chunk.css"
+  },
+  {
+    "revision": "50fe73a7b3703de6d2ee",
+    "url": "/Anonym/static/js/2.349284ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Anonym/static/js/2.349284ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0e1791e210efc9a7b91",
+    "url": "/Anonym/static/js/main.fa4c7198.chunk.js"
+  },
+  {
+    "revision": "070ffc4dc70e623d2af3",
+    "url": "/Anonym/static/js/runtime-main.8812744c.js"
+  },
+  {
+    "revision": "12604aaebc559d9dec8b6d6e417d2e3b",
+    "url": "/Anonym/static/media/sad.12604aae.jpg"
+  }
+]);
